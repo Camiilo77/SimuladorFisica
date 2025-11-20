5f4ec8c66158b0a18f5948e23b9ff8b364d4016d
Camilo Andres <camilorodriguez0309@gmail.com>
2025-11-20 02:59:27 -0500

    Arreglar1.2

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="emsim",
-    version="0.1",
+    name='emsim',
+    version='1.0',
     packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    package_dir={'': 'src'},
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pandas",
+        "plotly",
+        "streamlit"
+    ],
 )
