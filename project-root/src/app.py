4e2206c10dd0de7d35f1a362c75b6c3429530488
Camilo Andres <camilorodriguez0309@gmail.com>
2025-11-20 02:31:11 -0500

    correccion de errores

--- project-root/src/app.py
+++ project-root/src/app.py
@@ -1,7 +1,17 @@
+import sys
+import os
+
+# ============================
+#  AÑADIR /src AL PYTHONPATH
+# ============================
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))   # ruta: /src
+sys.path.append(BASE_DIR)
+
 import streamlit as st
 from emsim.ui.pages.plates_page import main as plates_page
 from emsim.ui.pages.sphere_page import main as sphere_page
 from emsim.ui.pages.cylinder_page import main as cylinder_page
+
 
 PAGES = {
     "Placas Paralelas": plates_page,
