8daf6165b9949018bcb74e0f4969de3212d11b69
Camilo Andres <camilorodriguez0309@gmail.com>
2025-11-20 02:52:38 -0500

    Arreglar paquetes

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="emsim",
-    version="0.1.0",
-    package_dir={"": "src"},
+    version="0.1",
     packages=find_packages(where="src"),
-    include_package_data=True,
+    package_dir={"": "src"},
 )
